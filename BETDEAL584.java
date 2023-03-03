4f73a5b922dd7ccf735f31df09ea078f9ca3fb38
ShubhangiXD <d.shubhangi1510@gmail.com>
2023-03-03 15:51:19 +0530

    modified:   BETDEAL584.java

--- BETDEAL584.java
+++ BETDEAL584.java
@@ -1,22 +1,17 @@
-/* package codechef; // don't place package name! */
-
 import java.util.*;
-import java.lang.*;
-import java.io.*;
 
-/* Name of the class has to be "Main" only if the class is public. */
 class BETDEAL584
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		Scanner sc = new Scanner (System.in);
-		System.out.println("Enter the number of test cases: ");
+		//System.out.println("Enter the number of test cases: ");
 		int T = sc.nextInt();
 		while (T>0) {
 		    T--;
-		    System.out.println("Enter the discount for store A: ");
+		    //System.out.println("Enter the discount for store A: ");
 		    int A = sc.nextInt();
-		    System.out.println("Enter the discount for store B: ");
+		    //System.out.println("Enter the discount for store B: ");
 		    int B = sc.nextInt();
 		    int amtA = 100 - ((100 * A)/100);
 		    int amtB = 200 - ((200 * B) /100);
@@ -27,5 +22,6 @@ class BETDEAL584
 		    else
 		    System.out.println("BOTH");
 		}
+        sc.close();
 	}
 }
