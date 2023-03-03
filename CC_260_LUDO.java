f80d50682bd907b00b6a4c9f8a1d8574e0789f0b
ShubhangiXD <d.shubhangi1510@gmail.com>
2023-03-03 16:54:17 +0530

    new file:   CC_260_LUDO.java

---
+++ CC_260_LUDO.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class CC_260_LUDO {
+    public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		while (T-- > 0) {
+		    int X = sc.nextInt();
+		    if (X==6)
+		    System.out.println("YES");
+		    else
+		    System.out.println("NO");
+		}
+        sc.close();
+	}    
+}
