a1a2619049567dc49ae25e24a655c17a61cabf50
ShubhangiXD <d.shubhangi1510@gmail.com>
2023-03-03 15:05:46 +0530

    new file:   AGEING299.java

---
+++ AGEING299.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class AGEING299
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		while (T>0) {
+		    int X = sc.nextInt();
+		    System.out.println(X-10);
+		    T--;
+		}
+		sc.close();
+	}
+}
