81d78c3ec8c77b44abbd860513dc14ee535dff98
ShubhangiXD <d.shubhangi1510@gmail.com>
2023-03-03 16:27:53 +0530

    new file:   CC_249_SQUATS.java

---
+++ CC_249_SQUATS.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class CC_249_SQUATS {
+    public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		while (T-- > 0) {
+		    int X = sc.nextInt();
+		    System.out.println(X*15);
+		}
+        sc.close();
+	}
+}
+
