5b93563a6a4fbc6505b272092d442a2f8f3d5609
ShubhangiXD <d.shubhangi1510@gmail.com>
2023-03-03 16:42:30 +0530

    new file:   CC_257_BIRIYANI.java

---
+++ CC_257_BIRIYANI.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class CC_257_BIRIYANI {
+    public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner (System.in);
+		int T = sc.nextInt();
+		while (T-- >0) {
+		    int X = sc.nextInt();
+		    int Y = sc.nextInt();
+		    System.out.println(X*Y);
+		}
+        sc.close();
+	}
+}
