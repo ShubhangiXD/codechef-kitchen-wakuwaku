c6eceed13f9b24af79abf0d24bb4fdf7fe5e13e0
ShubhangiXD <d.shubhangi1510@gmail.com>
2023-03-03 16:12:15 +0530

    new file:   CC_200_START01.java

---
+++ CC_200_START01.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+
+public class CC_200_START01 {
+    public static void main(String[] args) throws java.lang.Exception {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        System.out.print(N);
+        sc.close();
+    }
+}
