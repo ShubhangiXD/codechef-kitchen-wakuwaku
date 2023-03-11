f51731dee4d813781a724582d503acc24ee0c136
ShubhangiXD <d.shubhangi1510@gmail.com>
2023-03-11 18:13:29 +0530

    new file:   CC_279_AUDIBLE.java

--- CC_281_TALLER.java
+++ CC_281_TALLER.java
@@ -1,16 +1,17 @@
 import java.util.*;
+
 public class CC_281_TALLER {
-    public static void main (String[] args) throws java.lang.Exception
-	{
+	public static void main(String[] args) throws java.lang.Exception {
 		Scanner sc = new Scanner(System.in);
 		int T = sc.nextInt();
 		while (T-- > 0) {
-		    int X = sc.nextInt(); //ALice
-		    int Y = sc.nextInt(); //Bob
-		    if (X > Y)
-		    System.out.println("A");
-		    else
-		    System.out.println("B");
+			int X = sc.nextInt(); // ALice
+			int Y = sc.nextInt(); // Bob
+			if (X > Y)
+				System.out.println("A");
+			else
+				System.out.println("B");
 		}
+		sc.close();
 	}
 }
