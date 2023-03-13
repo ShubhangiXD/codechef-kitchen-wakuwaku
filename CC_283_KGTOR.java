94f97e9c3d6215da0da1db7ef28dede106378366
ShubhangiXD <d.shubhangi1510@gmail.com>
2023-03-13 14:08:29 +0530

    new file:   CC_283_KGTOR.java

---
+++ CC_283_KGTOR.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class CC_283_KGTOR {
+    public static void main(String[] args) throws java.lang.Exception {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            int N = sc.nextInt();
+            int M = sc.nextInt();
+            System.out.println((N * 5) + (M * 7));
+        }
+        sc.close();
+    }
+}
